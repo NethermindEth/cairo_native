8cb620ea0d258b1bc92efb7206849a84d43bc17a
Rodrigo <rodrodpino@gmail.com>
2024-04-04 10:03:31 +0100

    Minor fix

--- src/arch/x86_64.s
+++ src/arch/x86_64.s
@@ -1,8 +1,8 @@
 .text
 
 
-.global invoke_trampoline
-invoke_trampoline:
+.global _invoke_trampoline
+_invoke_trampoline:
     # rdi <- fn_ptr: extern "C" fn()
     # rsi <- args_ptr: *const u64
     # rdx <- args_len: usize
